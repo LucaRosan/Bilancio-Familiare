8eea37643f846dca435e3be0a5624ca41260b18d
LucaRosan <rosaticv@gmail.com>
2025-05-05 16:49:40 +0200

    Creazione del progetto

---
+++ bilancio-familiare/src/main/java/it/bilancio_familiare/BilancioFamiliareApplication.java
@@ -0,0 +1,13 @@
+package it.bilancio_familiare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BilancioFamiliareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BilancioFamiliareApplication.class, args);
+	}
+
+}
---
+++ bilancio-familiare/src/test/java/it/bilancio_familiare/BilancioFamiliareApplicationTests.java
@@ -0,0 +1,13 @@
+package it.bilancio_familiare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BilancioFamiliareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
