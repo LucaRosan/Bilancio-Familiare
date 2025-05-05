77e375427d46e01c9e3c5ac06cdea33d52532ff9
LucaRosan <rosaticv@gmail.com>
2025-05-05 17:17:33 +0200

    Model

--- bilancio-familiare/src/main/java/it/bilancio_familiare/controller/SalvaOperazioneController.java
+++ bilancio-familiare/src/main/java/it/bilancio_familiare/controller/SalvaOperazioneController.java
@@ -1,10 +1,17 @@
 package it.bilancio_familiare.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/")
 public class SalvaOperazioneController {
 
+
+
+    @GetMapping
+    public String renderPagina(){
+        return "index";
+    }
 }
---
+++ bilancio-familiare/src/main/java/it/bilancio_familiare/model/OperazioniFinanziarie.java
@@ -0,0 +1,27 @@
+package it.bilancio_familiare.model;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "bilancio_familiare")
+public class OperazioniFinanziarie {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private LocalDate dataOperazione;
+    private String descrizione;
+    private double importo;
+    private String tipoMovimento;
+}
